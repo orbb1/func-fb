30dd6c33f78000731e821ecede977178dbf273fc
maxuser <orbbmax@gmail.com>
2019-02-23 21:39:10 +0100

    initial fizzbuzz.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const hundred = [...Array(100)].map((_, i) => i);
+const withFizzBuzz = arr => arr.map(val => val % 3 === 0 && val % 5 === 0 ? 'FizzBuzz' : val);
+const withFizz = arr => arr.map(val => val % 3 === 0 ? 'Fizz' : val);
+const withBuzz = arr => arr.map(val => val % 5 === 0 ? 'Buzz' : val);
+const pipe = (...funcs) => arr => [...funcs].reduce((acc, func) => func(acc), arr);
+
 class App extends Component {
+  state = {
+    hundred: pipe(
+      withFizzBuzz,
+      withFizz,
+      withBuzz,
+    )(hundred),
+  }
+
   render() {
+    const generateKey = (i) => `${Math.random()}-${i}`;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          {this.state.hundred.map(v => <li key={generateKey(v)}>{v}</li>)}
+        </ul>
       </div>
     );
   }
