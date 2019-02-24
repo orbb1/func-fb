068281fff4fc101ba3a799d068266e866c7b9ff7
maxuser <orbbmax@gmail.com>
2019-02-24 20:54:07 +0100

    initial pagination hoc

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ const withFizz = arr => arr.map(val => val % 3 === 0 ? 'Fizz' : val);
 const withBuzz = arr => arr.map(val => val % 5 === 0 ? 'Buzz' : val);
 const pipe = (...funcs) => arr => [...funcs].reduce((acc, func) => func(acc), arr);
 
+const WithPagination = ({items, amountPerPage, pageNumber, children}) => {
+  const newArr = items.slice(pageNumber, amountPerPage*(pageNumber));
+  const childrenWithProps = React.Children.map(children, child =>
+    React.cloneElement(child, { items: newArr })
+  );
+  return (<div>{childrenWithProps}</div>);
+}
+
+const List = ({items}) => (
+  <ul>
+    {items.map(item => <ListItem text={item} />)}
+  </ul>
+)
+
 class App extends Component {
   state = {
     hundred: pipe(
@@ -16,13 +30,12 @@ class App extends Component {
       withBuzz,
     )(hundred),
   }
-
   render() {
     return (
       <div className="App">
-        <ul>
-          {this.state.hundred.map(item => <ListItem text={item} />)}
-        </ul>
+        <WithPagination items={this.state.hundred} amountPerPage={20} pageNumber={1}>
+          <List/>
+        </WithPagination>
       </div>
     );
   }
